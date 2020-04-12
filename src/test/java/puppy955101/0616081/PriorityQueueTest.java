a8813f467f3f962871f1186811bdcffb8573d6fa
User <puppy955101@gmail.com>
2020-04-12 19:18:07 +0800

    init

--- src/test/java/puppy955101/0616081/PriorityQueueTest.java
+++ src/test/java/puppy955101/0616081/PriorityQueueTest.java
@@ -12,15 +12,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(Parameterized.class)
 public class PriorityQueueTest {
     private int[] INPUT;
+    private int[] OUTPUT;
 
+    public PriorityQueueTest(int[] input, int[] output){
+        this.INPUT = input;
+        this.OUTPUT = output;
+    }
     @Parameterized.Parameters
     public static Collection Testcase(){
-        return Arrays.asList(new int[][] {
-                {9,4,6,8,7,10},{951,3154,1,999,888,7},{55,33,22,77,44,99,11},{987,321,654,876,543,999,0},{456,123,789,1234,7890,4567}
+        return Arrays.asList(new int[][][] {
+                {{9,4,6,8,7,10},{4,6,7,8,9,10}},
+                {{951,3154,1,999,888,7},{1,7,888,951,999,3154}},
+                {{55,33,22,77,44,99,11},{11,22,33,44,55,77,99}},
+                {{987,321,654,876,543,999,0},{0,321,543,654,876,987,999}},
+                {{456,123,789,1234,7890,4567},{123,456,789,1234,4567,7890}}
         });
-    }
-    public PriorityQueueTest(int[] input){
-        this.INPUT = input;
     }
     @Test
     public void test(){
@@ -29,38 +35,10 @@ public class PriorityQueueTest {
             Q.add(i);
 //            System.out.println(i);
         }
-        int out[] = new int[INPUT.length];
         int x=0;
         while(!Q.isEmpty()){
-            out[x] = Q.poll();
-//            System.out.println(out[x]);
+            assertEquals(OUTPUT[x],Q.poll());
             x++;
-        }
-        int tmp[][] = new int[][]{{4,6,7,8,9,10},{1,7,888,951,999,3154},{11,22,33,44,55,77,99},{0,321,543,654,876,987,999},{123,456,789,1234,4567,7890}};
-        if(INPUT[0]==9){
-            for (int i=0;i<out.length;i++) {
-                assertEquals(tmp[0][i],out[i]);
-            }
-        }
-        else if(INPUT[0]==951){
-            for (int i=0;i<out.length;i++) {
-                assertEquals(tmp[1][i],out[i]);
-            }
-        }
-        else if(INPUT[0]==55){
-            for (int i=0;i<out.length;i++) {
-                assertEquals(tmp[2][i],out[i]);
-            }
-        }
-        else if(INPUT[0]==987){
-            for (int i=0;i<out.length;i++) {
-                assertEquals(tmp[3][i],out[i]);
-            }
-        }
-        else if(INPUT[0]==456){
-            for (int i=0;i<out.length;i++) {
-                assertEquals(tmp[4][i],out[i]);
-            }
         }
     }
     @Test
